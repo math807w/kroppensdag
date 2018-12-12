e514f938cefb14ecf58222355e6d86b5967d7728
Mathias Hammer <math807w@edu.eal.dk>
2018-12-12 17:32:11 +0100

    Speakers + footer social links

--- js/script.js
+++ js/script.js
@@ -82,19 +82,20 @@ document.addEventListener("DOMContentLoaded", function() {
                 header.style.top = "-" + header.offsetHeight + "px";
             }
         }
+        /* -------------------- PEEKABOO -------------------- */
+
+    function yScrollHandler(){
+        var win = document.getElementById("peekaboo");
+        if((window.pageYOffset + window.innerHeight) >= document.body.offsetHeight){
+        //win.style.webkitTransition = "right 0.7s ease-in-out 0s";
+        win.style.transition = "right 0.7s ease-in-out 0s";
+        win.style.right = "10%";
+        } else {
+        win.style.transition = "right 0.7s ease-in-out 0s";
+        win.style.right = "-452px";
+        }
+    }
+    window.onscroll = yScrollHandler;
+    
 });
 
-/* -------------------- PEEKABOO -------------------- */
-
-function yScrollHandler(){
-    var win = document.getElementById("peekaboo");
-    if((window.pageYOffset + window.innerHeight) >= document.body.offsetHeight){
-	//win.style.webkitTransition = "right 0.7s ease-in-out 0s";
-	win.style.transition = "right 0.7s ease-in-out 0s";
-	win.style.right = "0px";
-    } else {
-	win.style.transition = "right 0.7s ease-in-out 0s";
-	win.style.right = "-452px";
-    }
-}
-window.onscroll = yScrollHandler;
