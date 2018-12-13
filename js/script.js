2c4564fe6d870da1c8efd93160bf5ec893d34136
Mathias Hammer <math807w@edu.eal.dk>
2018-12-13 15:52:37 +0100

    Peekaboo overflow fix

--- js/script.js
+++ js/script.js
@@ -91,7 +91,11 @@ document.addEventListener("DOMContentLoaded", function() {
         var win = document.getElementById("peekaboo");
         if((window.pageYOffset + window.innerHeight) >= document.body.offsetHeight){
         win.style.transition = "right 0.7s ease-in-out 0s";
-        win.style.right = "0";
+        win.style.display = "block";        
+        setTimeout(
+            function(){
+                win.style.right = "0";
+            }, 1);
         } 
     }
     window.onscroll = yScrollHandler;
