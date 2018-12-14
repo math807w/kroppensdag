f59177fdc1fb63aa40718d95d6c380018e0e94d1
Mathias Hammer <math807w@edu.eal.dk>
2018-12-14 11:09:19 +0100

    styling + sticky demo

---
+++ sticky.js
@@ -0,0 +1,16 @@
+    document.addEventListener('scroll', fixedHeader);
+
+        function fixedHeader(){
+            var header = document.getElementById('header'),
+                section = document.getElementById('fp-section'),
+                dst = document.documentElement.scrollTop,
+                bst = document.body.scrollTop;
+
+            if (dst >= section.offsetHeight){
+                header.style.top = "0px";
+                header.style.position = "fixed";
+            }
+            else if(dst < section.offsetHeight){
+                header.style.top = "-" + header.offsetHeight + "px";
+            }
+        }
